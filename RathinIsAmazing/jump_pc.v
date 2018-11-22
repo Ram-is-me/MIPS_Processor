fa713b1a9f10b72e9bc5fd7da106b4a86a94dcbd
Rathin Bhargava <rathin7@gmail.com>
2018-11-23 01:14:45 +0530

    My own folder

--- RathinIsAmazing/jump_pc.v
+++ RathinIsAmazing/jump_pc.v
@@ -4,4 +4,13 @@ module jump_pc(in_pc,jumper,pc_jump,out_pc);
   input [25:0]jumper;
   output reg [31:0]pc_jump,out_pc;
 
+  always@(in_pc)
+  begin
+    out_pc = in_pc+4;
+  end
+
+  always@(out_pc,jumper)
+  begin
+    pc_jump = {out_pc[31:29],jumper<<2};
+  end
 endmodule
